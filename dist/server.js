6fd8e6b1a9975b817b904b3e44ae54c0770d8d48
cristiano-reis <crisreis2@live.com>
2021-08-08 00:15:49 -0300

    ormconfig  ssl

--- dist/server.js
+++ dist/server.js
@@ -3,8 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
 require("./config/env");
+require("reflect-metadata");
 var express_1 = __importDefault(require("express"));
 require("./database");
 var routes_1 = __importDefault(require("./routes"));
