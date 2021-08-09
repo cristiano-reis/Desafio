1b91e466083a8e144a09c127fa60f74ed05d34d1
cristiano-reis <crisreis2@live.com>
2021-08-08 23:40:22 -0300

    dist criado

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./config/env");
+require("reflect-metadata");
+var express_1 = __importDefault(require("express"));
+require("./database");
+var routes_1 = __importDefault(require("./routes"));
+var app = express_1.default();
+app.use(express_1.default.json());
+app.use(routes_1.default);
+app.listen(process.env.PORT || 3333, function () { console.log('Server Ligado!'); });
